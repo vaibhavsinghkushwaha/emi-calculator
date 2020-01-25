6cfcc663b6e3e2411a95b84993e3654975116d19
vaibhavsinghkushwaha <vkushwaha542@rku.ac.in>
2020-01-25 12:15:09 +0530

    Disallow negative value for all parameters

--- src/emi.js
+++ src/emi.js
@@ -14,6 +14,12 @@ function Loan (amount, installmentsNumber, interestRate) {
      !interestRate) {
     throw new Error(`wrong parameters: ${amount} ${installmentsNumber} ${interestRate}`)
   }
+  // function Loan
+  if (!amount || amount <= 0 ||
+   !installmentsNumber || installmentsNumber <= 0 ||
+   !interestRate || interestRate <= 0) {
+    throw new Error(`wrong parameters: ${amount} ${installmentsNumber} ${interestRate}`)
+  }
 
   const installments = []
   let interestSum = 0
